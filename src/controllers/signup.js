1cbc0b8e27b1bae9ac4f77ccb9a54c6547057020
prateek-10 <prateeksibu333@gmail.com>
2024-05-14 09:57:33 +0530

    signup_update

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,8 +1,14 @@
 const userService = require("../services/signup");
+const User = require("../models/user");
 
 async function createUser(req, res) {
   try {
     const userData = req.body;
+    const { email } = userData;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
     const user = await userService.createUser(userData);
     res.status(201).json({ user: user, message: "User created successfully" });
   } catch (error) {
